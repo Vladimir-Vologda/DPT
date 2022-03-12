e30b5aaef2d59ffa6c11bc9ab3982f9744c896cc
Vladimir-Vologda <vologda2018@inbox.ru>
2022-03-12 18:02:33 +0300

    Application connection "Accounts"

--- main/settings.py
+++ main/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #   --------------------------------
+    'rest_framework',
+    'phonenumber_field',
+    #   --------------------------------
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
